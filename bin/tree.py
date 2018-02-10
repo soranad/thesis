0bf239c3516befd60a719a82cc07849d1eafce81
Soranad Chaipet <soranad.chaipet@gmail.com>
2018-02-10 19:18:34 +0700

    decress number of send package

--- bin/tree.py
+++ bin/tree.py
@@ -74,7 +74,7 @@ def emptyNet():
 	info( "\n\n*** gennerate send rate \n" )
 
 	sendRate = int(sys.argv[5])
-	numberOfSends = 1000
+	numberOfSends = 500
 	beta = 1.0/sendRate
 	Y = np.random.exponential(beta, numberOfSends)
 
