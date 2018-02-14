2df446aa4595885373006be94c82b93e4add17d6
Soranad Chaipet <soranad.chaipet@gmail.com>
2018-02-14 18:53:32 +0700

    fix static for ONOS

--- bin/static-host-send-run.sh
+++ bin/static-host-send-run.sh
@@ -1,4 +1,4 @@
-controller_IP="192.168.1.11"
+controller_IP="192.168.1.22"
 username="nick"
 password="password"
 interface="enp7s0"
@@ -20,12 +20,12 @@ do
 
 	# number of switch
 	# for no_switch in 010 020 030
-	for no_switch in 100 200 300 400 500
+	for no_switch in 100 200 300
 	do
 	
 		# send rate (flow per sec)
 		# for send_rate in 025 050 075 100 125 150 175 200 225 250 275 300
-		for send_rate in 025 050 075 100 125 150 175 200 225 250 275 300
+		for send_rate in 025 050 075 100 125 150 175 200 225 250 275 300 325 350 375 400
 		do
 
 			sudo mn -c
@@ -44,7 +44,7 @@ do
 
 			sudo sshpass -p $password ssh $username@$controller_IP 'rm cpu-mem-uses.txt'
 			sudo sshpass -p $password ssh $username@$controller_IP './start-controller.sh' &
-			sudo sshpass -p $password ssh $username@$controller_IP './start-cpu-mem-capture >> cpu-mem-uses.txt' &
+			sudo sshpass -p $password ssh $username@$controller_IP './start-cpu-mem-capture.sh' &
 			
 			echo "wait for start topology"
 			for delay in $(seq 1 $delay_after_start_controller)
@@ -58,6 +58,8 @@ do
 			
 			sudo python tree-static-host-send.py $no_switch $no_host $interface $no_capture_package $send_rate $sent_long $controller_IP ../results/tree-$no_switch-sw-$send_rate-ps-$repeat_no
 			sudo sshpass -p $password scp $username@$controller_IP:cpu-mem-uses.txt ../results/tree-$no_switch-sw-$send_rate-ps-$repeat_no
+
+			sudo sshpass -p $password ssh $username@$controller_IP './stop-controller.sh' &
 
 			echo "" >> ../results/result.txt
 			echo "----- tree-$no_switch-sw-$send_rate-ps-$repeat_no/ -----" >> ../results/result.txt
