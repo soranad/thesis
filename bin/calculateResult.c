87fb7007b6da9e39e0efb7144b64d60db6876119
Soranad Chaipet <soranad.chaipet@gmail.com>
2018-02-20 16:52:13 +0700

    fix show results in calculateResult

--- bin/calculateResult.c
+++ bin/calculateResult.c
@@ -47,11 +47,17 @@ int main(int argc, char **argv)
 				fclose(inF);
 				
 				if(isSendSuccess == 1){
-					if(count == 0 || delay < minDelay){
-					minDelay = delay;
-					}
-					if(count == 0 || delay > maxDelay){
+					if(count == 0){
+						minDelay = delay;
 						maxDelay = delay;
+					}
+					else{
+						if( delay < minDelay){
+							minDelay = delay;
+						}
+						if( delay > maxDelay){
+							maxDelay = delay;
+						}
 					}
 					totalDelay += delay;
 					count++;
@@ -64,16 +70,18 @@ int main(int argc, char **argv)
 		}
 		closedir(d);
 	}
-
-	printf("\n LATENCY USE \n");
-	printf("total:%d   send error:%d \n", count, sendErrorCount);
+	
+	double avgDelay = 0.0;
 	if(count > 0){
-		printf("%lf,%lf,%lf\n",minDelay,maxDelay,totalDelay/count);
-	}
-	else{
-		printf("%lf,%lf,%lf\n",minDelay,maxDelay,0.0);
+		totalDelay/count;
 	}
 
+	printf("\n\x1b[32m#####  LATENCY  #####\x1b[0m\n");
+	printf("ping success : %10d\n", count);
+	printf("ping error   : %10d\n", sendErrorCount);
+	printf("ping min   : %12.3lf\n", minDelay);
+	printf("ping max   : %12.3lf\n", maxDelay);
+	printf("ping avg   : %12.3lf\n", totalDelay/count);
 
 	// calculate controller cpu & mem used
 	FILE * file;
@@ -84,26 +92,24 @@ int main(int argc, char **argv)
 
 	double cpuMin;
 	double cpuMax;
-	double cpuAvg;
+	double sumCPU = 0;
 	double memMin;
 	double memMax;
-	double memAvg;
+	double sumMem = 0;
+	int numLine = 0;
 
 	if (file) {
 		char processName[128];
-		double cpu;
-		double mem;
-		int numLine = 0;
+		double cpu ;
+		double mem ;
 
 		while(fscanf(file, "%s %lf, %lf", processName, &cpu, &mem) != EOF){
 			// printf("%s %lf, %lf\n", processName, cpu, mem);
 			if(numLine == 0){
 				cpuMin = cpu;
 				cpuMax = cpu;
-				cpuAvg = cpu;
 				memMin = mem;
 				memMax = mem;
-				memAvg = mem;
 			}
 			else{
 				if(cpu < cpuMin){
@@ -118,72 +124,30 @@ int main(int argc, char **argv)
 				if(mem > memMax){
 					memMax = mem;
 				}
-				cpuAvg += cpu;
-				memAvg += mem;
 			}
 			numLine++;
+			sumCPU += cpu;
+			sumMem += mem;
 		}
-		cpuAvg = cpuAvg/numLine;
-		memAvg = memAvg/numLine;
 		fclose(file);
 	}
-	printf("\n CONTROLLER USE \n");
-	printf("cpu : %.2lf %.2lf %.2lf\n", cpuMin, cpuMax, cpuAvg);
-	printf("mem : %.2lf %.2lf %.2lf\n", memMin, memMax, memAvg);
 
+	double cpuAvg = 0.0;
+	double memAvg = 0.0;
+	if(numLine > 0){
+		cpuAvg = sumCPU/numLine;
+		memAvg = sumMem/numLine;
+	}
+
+	printf("\n\x1b[32m#####  CONTROLLER USE  #####\x1b[0m\n");
+	printf("cpu use min    : %12.3lf\n", cpuMin);
+	printf("cpu use max    : %12.3lf\n", cpuMax);
+	printf("cpu use avg    : %12.3lf\n", cpuAvg);
+	printf("memory use min : %12.3lf\n", memMin);
+	printf("memory use max : %12.3lf\n", memMax);
+	printf("memory use avg : %12.3lf\n", memAvg);
 
 	// calculate control package
-	strcpy(part, argv[1]);
-	strcat(part, "cap.csv");
-	file = fopen(part, "r");
-
-	int packageTypes[6][35];
-	int i;
-	int j;
-	long long int totalPayload = 0;
-	int packageCount = 0;
-
-	for(i=0; i<6; i++){
-		for(j=0; j<35; j++){
-			packageTypes[i][j] = 0;
-		}
-	}
-
-	if (file) {
-
-		
-		char* line = NULL;
-		size_t len = 0;
-		getline(&line, &len, file);
-
-		int index;
-		int sIP1,sIP2,sIP3,sIP4;
-		int dIP1,dIP2,dIP3,dIP4;
-		int sourcePort;
-		int destinstionPort;
-		double time;
-		int winsize;
-		int payload;
-		int type;
-		int version;
-
-
-		while(fscanf(file, "%d,%lf,%d.%d.%d.%d,%d,%d.%d.%d.%d,%d,%d,%d,%d,%d", &index, &time, &sIP1,&sIP2,&sIP3,&sIP4, &sourcePort, &dIP1,&dIP2,&dIP3,&dIP4, &destinstionPort, &winsize, &payload, &type, &version) != EOF){
-			// printf("%d %d %d\n",index, type, version);
-			if( type >= 0 && type < 10 && version >= 0 && version < 60 ){
-				packageCount++; 
-				packageTypes[type][version]++;
-				totalPayload += payload;
-			}
-		}
-		fclose(file);
-	}
-
-
-	printf("\n NETWORK USE \n");
-	printf("number of package : %d \n", packageCount); 
-	printf("sum of payload : %lld \n\n", totalPayload);
-
 	char * nameList[][6] = {
 		{"Type\\Version","OFP 1.0","OFP 1.1","OFP 1.2","OFP 1.3","OFP 1.4"},
 		{"0","Hello","Hello","Hello","Hello","Hello"},
@@ -223,27 +187,59 @@ int main(int argc, char **argv)
 		{"34","-","-","-","-","BundleAddMessage"}
 	};
 
+	strcpy(part, argv[1]);
+	strcat(part, "cap.csv");
+	file = fopen(part, "r");
+
+	int packageTypes[6][35];
+	int i;
+	int j;
+	long long int totalPayload = 0;
+	int packageCount = 0;
+
+	for(i=0; i<6; i++){
+		for(j=0; j<35; j++){
+			packageTypes[i][j] = 0;
+		}
+	}
+
+	if (file) {
+
+		char* line = NULL;
+		size_t len = 0;
+		getline(&line, &len, file);
+
+		int index;
+		int sIP1,sIP2,sIP3,sIP4;
+		int dIP1,dIP2,dIP3,dIP4;
+		int sourcePort;
+		int destinstionPort;
+		double time;
+		int winsize;
+		int payload;
+		int type;
+		int version;
+
+		while(fscanf(file, "%d,%lf,%d.%d.%d.%d,%d,%d.%d.%d.%d,%d,%d,%d,%d,%d", &index, &time, &sIP1,&sIP2,&sIP3,&sIP4, &sourcePort, &dIP1,&dIP2,&dIP3,&dIP4, &destinstionPort, &winsize, &payload, &type, &version) != EOF){
+			// printf("%d %d %d\n",index, type, version);
+			if( type >= 0 && type < 10 && version >= 0 && version < 60 ){
+				packageCount++; 
+				packageTypes[type][version]++;
+				totalPayload += payload;
+			}
+		}
+		fclose(file);
+	}
+
+	printf("\n\x1b[32m#####  NETWORK USE  #####\x1b[0m\n");
+	printf("number of package : %12d\n", packageCount); 
+	printf("payload sumation  : %12lld\n", totalPayload);
+
 	for(j=0; j<35; j++){
 		for(i=0; i<6; i++){
 			if(packageTypes[i][j] > 0){
-				printf("%-15s %-20s %12d \n",nameList[0][i],nameList[j][i], packageTypes[i][j]);
+				printf("%-15s %-15s : %12d \n",nameList[0][i],nameList[j][i], packageTypes[i][j]);
 			}
-			// if(packageTypes[i][j] > 0){
-			// 	if(j == 0){
-			// 		printf("\x1b[32m%18s\x1b[35m:\x1b[31m%-8d\x1b[0m",nameList[j][i], packageTypes[i][j]);
-			// 	}
-			// 	else{
-			// 		printf("\x1b[32m%18s\x1b[35m:\x1b[31m%-8d\x1b[0m",nameList[j+1][i], packageTypes[i][j]);
-			// 	}
-			// }
-			// else{
-			// 	if(j == 0){
-			// 		printf("\x1b[32m%18s\x1b[35m:\x1b[33m%-8d\x1b[0m",nameList[j][i], packageTypes[i][j]);
-			// 	}
-			// 	else{
-			// 		printf("\x1b[32m%18s\x1b[35m:\x1b[33m%-8d\x1b[0m",nameList[j+1][i], packageTypes[i][j]);
-			// 	}
-			// }
 		}
 	}
 	printf("\n");
